ba395ccf1f96a6b8c31326561b1b79729a478c61
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-07-01 15:59:27 -0300

    fix: colocando o retorno como response entity

--- src/main/java/com/example/cruduserwithspring/controller/UserController.java
+++ src/main/java/com/example/cruduserwithspring/controller/UserController.java
@@ -2,10 +2,11 @@ package com.example.cruduserwithspring.controller;
 
 import com.example.cruduserwithspring.model.User;
 import com.example.cruduserwithspring.service.UserService;
-import com.example.cruduserwithspring.service.UserServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -20,14 +21,14 @@ public class UserController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping(value = "/")
-    public Collection<User> getAll() {
+    public ResponseEntity<Collection<User>> getAll() {
         this.logger.debug("Get all users");
-        return this.user.getAllUsers();
+        return new ResponseEntity<>(this.user.getAllUsers(), HttpStatus.OK);
     }
 
     @PostMapping(value = "/")
-    public User addUser(@RequestBody User user) {
+    public ResponseEntity<User> addUser(@RequestBody User user) {
         this.logger.debug("Add a new user");
-        return this.user.addUser(user);
+        return new ResponseEntity<>(this.user.addUser(user), HttpStatus.OK);
     }
 }
